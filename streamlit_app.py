9c8f540fb783ccd4250e1456dcab8389c09df4d4
AleeyaHayfa <121542712+AleeyaHayfa@users.noreply.github.com>
2025-01-05 14:12:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,14 @@ st.title('🎈JomPredict')
 
 st.write('This is an app testing for Assignment CPT316!')
 
-df = pd.read_csv("https://raw.githubusercontent.com/AleeyaHayfa/shiny-broccoli/refs/heads/master/diabetes_binary_5050split_health_indicators_BRFSS2015.csv")
-df
+with st.expander('Data'):
+  df = pd.read_csv("https://raw.githubusercontent.com/AleeyaHayfa/shiny-broccoli/refs/heads/master/diabetes_binary_5050split_health_indicators_BRFSS2015.csv")
+  df
 
-st.write('**X**')
-x_raw = df.drop('Diabetes_binary', axis=1)
-x_raw
+  st.write('**X**')
+  x_raw = df.drop('Diabetes_binary', axis=1)
+  x_raw
 
-st.write('Y')
-y_raw = df.Diabetes_binary
-y_raw
+  st.write('Y')
+  y_raw = df.Diabetes_binary
+  y_raw
