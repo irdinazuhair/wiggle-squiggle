20a8bb0162c1cbf1105662aa99cc77395b21285f
AleeyaHayfa <121542712+AleeyaHayfa@users.noreply.github.com>
2025-01-05 14:06:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,11 @@ st.write('This is an app testing for Assignment CPT316!')
 
 df = pd.read_csv("https://raw.githubusercontent.com/AleeyaHayfa/shiny-broccoli/refs/heads/master/diabetes_binary_5050split_health_indicators_BRFSS2015.csv")
 df
+
+st.write('**X**')
+x_raw = df.drop('Diabetes_binary', axis=1)
+x_raw
+
+st.write('Y')
+y_raw = df.Diabetes_binary
+y_raw
