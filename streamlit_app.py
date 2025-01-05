b9c7a73c2f01898b7d923fea8f50201b7df618f0
AleeyaHayfa <121542712+AleeyaHayfa@users.noreply.github.com>
2025-01-05 14:04:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈JomPredict')
 
 st.write('This is an app testing for Assignment CPT316!')
+
+df = pd.read_csv("https://raw.githubusercontent.com/AleeyaHayfa/shiny-broccoli/refs/heads/master/diabetes_binary_5050split_health_indicators_BRFSS2015.csv")
+df
