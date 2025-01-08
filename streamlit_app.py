f692a149a92722c5689cca8e04663ffce7262273
eatyourbrocoli <121542712+AleeyaHayfa@users.noreply.github.com>
2025-01-08 22:30:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,86 +3,133 @@ import numpy as np
 import streamlit as st
 from sklearn.ensemble import RandomForestClassifier
 
-with st.expander('Data'):
-    # Load the dataset
-    df = pd.read_csv("https://raw.githubusercontent.com/AleeyaHayfa/shiny-broccoli/refs/heads/master/Social_Housing_cleaned.csv")
-    st.write('Dataset:')
-    st.dataframe(df)
+class HousingHomelessnessPrediction:
+    def __init__(self, data_url):
+        self.data_url = data_url
+        self.df = None
+        self.x_raw = None
+        self.y_raw = None
+        self.clf = RandomForestClassifier()
 
-    # Separate features (X) and target (Y)
-    st.write('**X (Features)**')
-    x_raw = df.drop('AtRiskOfOrExperiencingHomelessnessFlag', axis=1)
-    st.dataframe(x_raw)
+    def load_data(self):
+        self.df = pd.read_csv(self.data_url)
+        self.x_raw = self.df.drop('AtRiskOfOrExperiencingHomelessnessFlag', axis=1)
+        self.y_raw = self.df['AtRiskOfOrExperiencingHomelessnessFlag']
 
-    st.write('**Y (Target)**')
-    y_raw = df['AtRiskOfOrExperiencingHomelessnessFlag']
-    st.write(y_raw)
+    def preprocess_target(self):
+        target_mapper = {'Yes': 1, 'No': 0}
+        return self.y_raw.apply(lambda val: target_mapper[val])
 
-with st.expander('Data Visualization'):
-  st.scatter_chart(data=df, x='PeopleonApplication', y='FamilyType', color='AtRiskOfOrExperiencingHomelessnessFlag')
+    def encode_features(self, input_details):
+        encode_cols = ['FamilyType', 'DisabilityApplicationFlag']
+        encoded_df = pd.get_dummies(input_details, columns=encode_cols)
+        return encoded_df
 
-# Data prepration (user input)
-with st.sidebar:
-  st.header('Input features')
-  Family = st.selectbox('Family Type', (
-    'Single Person', 
-    'Single Parent, 1 Child', 
-    'Single Parent, 2 Children', 
-    'Single Parent, >2 Children', 
-    'Couple Only', 
-    'Couple, 1 Child', 
-    'Couple, 2 Children', 
-    'Couple, >2 Children', 
-    'Single Person Over 55', 
-    'Couple Only Over 55', 
-    'Other'
-))
-  DisabilityFlag = st.selectbox('Disability', ('Yes', 'No'))
-  TotalPeople = st.slider('Total People on application', 1, 12, 2)
-  TotalMonths = st.slider('Total months you have been register', 0, 239, 23)
-  # 1.0 mininum, 12.0 maximum, 1.681 average (same as other)
+    def train_model(self, x, y):
+        self.clf.fit(x, y)
 
-# user input into dataframe
-# Create a DataFrame for the input features
-  data = {'FamilyType': Family,
-          'MonthsonHousingRegister': TotalMonths,
-          'DisabilityApplicationFlag': DisabilityFlag,
-          'PeopleonApplication': TotalPeople,
-         }
-  input_df = pd.DataFrame(data, index=[0])
-  input_details = pd.concat([input_df, x_raw], axis=0) #combine input features with penguin features
-  # input details is user input
+    def predict(self, input_row):
+        prediction = self.clf.predict(input_row)
+        prediction_proba = self.clf.predict_proba(input_row)
+        return prediction, prediction_proba
 
-with st.expander('Input features'):
-  st.write('**Input User**')
-  input_df
-  st.write('**Combined Housing data**') #combine original dgn user input
-  input_details
+class StreamlitApp:
+    def __init__(self, model):
+        self.model = model
 
-# Encode X 
-encode = ['FamilyType', 'DisabilityApplicationFlag']
-df_house = pd.get_dummies(input_details, prefix=encode)
+    def display_data(self):
+        with st.expander('Data'):
+            st.write('Dataset:')
+            st.dataframe(self.model.df)
+            st.write('**X (Features)**')
+            st.dataframe(self.model.x_raw)
+            st.write('**Y (Target)**')
+            st.write(self.model.y_raw)
 
-x = df_house[1:] #ignore first row but use everything after first row
-input_row = df_house[:1] #use only first row
+    def display_visualisation(self):
+        with st.expander('Data Visualization'):
+            st.scatter_chart(data=self.model.df, x='PeopleonApplication', y='FamilyType', color='AtRiskOfOrExperiencingHomelessnessFlag')
 
-# Encode y
-target_mapper = {'Yes': 1,
-                 'No': 0
-                 }
-def target_encode(val):
-  return target_mapper[val]
+    def get_user_input(self):
+        with st.sidebar:
+            st.header('Input features')
+            Family = st.selectbox('Family Type', (
+                'Single Person', 
+                'Single Parent, 1 Child', 
+                'Single Parent, 2 Children', 
+                'Single Parent, >2 Children', 
+                'Couple Only', 
+                'Couple, 1 Child', 
+                'Couple, 2 Children', 
+                'Couple, >2 Children', 
+                'Single Person Over 55', 
+                'Couple Only Over 55', 
+                'Other'))
+            DisabilityFlag = st.selectbox('Disability', ('Yes', 'No'))
+            TotalPeople = st.slider('Total People on application', 1, 12, 2)
+            TotalMonths = st.slider('Total months you have been register', 0, 239, 23)
 
-y = y_raw.apply(target_encode)
-#y #ni yang encoded
-#y_raw # original
+            input_data = {
+                'FamilyType': Family,
+                'MonthsonHousingRegister': TotalMonths,
+                'DisabilityApplicationFlag': DisabilityFlag,
+                'PeopleonApplication': TotalPeople,
+            }
+            return pd.DataFrame(input_data, index=[0])
 
+    def display_input(self, input_df, input_details):
+        with st.expander('Input features'):
+            st.write('**Input User**')
+            st.dataframe(input_df)
+            st.write('**Combined Housing data**')
+            st.dataframe(input_details)
 
-with st.expander('Data preparation'):
-  st.write('**Encoded X (input housing)**')
-  input_row
-  st.write('**Encoded y**')
-  y
-  # y_raw #compare dgn original nk tgk betul ke tak
+    def display_prepared_data(self, input_row, y):
+        with st.expander('Data preparation'):
+            st.write('**Encoded X (input housing)**')
+            st.dataframe(input_row)
+            st.write('**Encoded y**')
+            st.write(y)
 
+    def display_predictions(self, prediction, prediction_proba):
+        housing_homeless = np.array(['Yes', 'No'])
+        df_prediction_proba = pd.DataFrame(prediction_proba, columns=['No', 'Yes'])
 
+        st.subheader('Predicted Homelessness')
+        st.dataframe(
+            df_prediction_proba,
+            column_config={
+                'Yes': st.column_config.ProgressColumn(
+                    'Yes', format='%f', width='medium', min_value=0, max_value=1),
+                'No': st.column_config.ProgressColumn(
+                    'No', format='%f', width='medium', min_value=0, max_value=1),
+            },
+            hide_index=True
+        )
+        st.success(str(housing_homeless[prediction][0]))
+
+# Main Execution
+model = HousingHomelessnessPrediction("https://raw.githubusercontent.com/AleeyaHayfa/shiny-broccoli/refs/heads/master/Social_Housing_cleaned.csv")
+model.load_data()
+y_encoded = model.preprocess_target()
+
+app = StreamlitApp(model)
+
+app.display_data()
+app.display_visualisation()
+
+input_df = app.get_user_input()
+input_details = pd.concat([input_df, model.x_raw], axis=0)
+
+app.display_input(input_df, input_details)
+
+df_house = model.encode_features(input_details)
+x_encoded = df_house[1:]
+input_row_encoded = df_house[:1]
+
+app.display_prepared_data(input_row_encoded, y_encoded)
+
+model.train_model(x_encoded, y_encoded)
+
+prediction, prediction_proba = model.predict(input_row_encoded)
+app.display_predictions(prediction, prediction_proba)
